0eb90cd5ab591b771a674f2c0a11c03f013f469b
Sajid <sajidpalagiri1@gmail.com>
2024-08-22 23:24:47 +0530

    First App

--- FirstApp/ContentView.swift
+++ FirstApp/ContentView.swift
@@ -7,17 +7,34 @@
 
 import SwiftUI
 
-struct ContentView: View {
-    var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
-    }
+enum Emoji: String, CaseIterable {
+    case 🤔,💚,😌,😢
 }
+struct ContentView: View {
+    
+    @State var selection: Emoji = .🤔
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text(selection.rawValue)
+                    .font(.system(size: 150))
+                        
+                        Picker("Select Emoji", selection: $selection) {
+                                        ForEach(Emoji.allCases, id: \.self) { emoji in
+                                            Text(emoji.rawValue)
+                    }
+                }
+                .pickerStyle(.segmented)
+            }
+            .navigationTitle("Emoji Picker")
+            .padding()
+        }
+        
+        
+        }
+    }
+
 
 #Preview {
     ContentView()
